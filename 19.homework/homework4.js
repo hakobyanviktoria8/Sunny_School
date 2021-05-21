d689d4d4a1b06c02211961b1d8f896dd68da28ef
Viktorya Hakobyan <hakobyanviktorya8@gmail.com>
2021-05-22 01:58:54 +0400

    19hw

--- 19.homework/homework4.js
+++ 19.homework/homework4.js
@@ -4,7 +4,8 @@
 const fs = require("fs").promises;
 
 async function returnPromise(path) {
-    let data =  (await fs.readFile(path)).toString().split(" ").map(x => x[0].toUpperCase() + x.slice(1)).join(" ");
+    let data =  (await fs.readFile(path)).toString().split(" ")
+        .map(x => x[0].toUpperCase() + x.slice(1)).join(" ");
     console.log(data);
     await fs.writeFile(path, data)
 }
