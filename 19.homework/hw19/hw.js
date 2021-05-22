b6c46ec6f9f4f17870b7fa0053f14ea08eba8287
Viktorya Hakobyan <hakobyanviktorya8@gmail.com>
2021-05-22 17:03:57 +0400

    hw19 full

--- 19.homework/hw19/hw.js
+++ 19.homework/hw19/hw.js
@@ -1,4 +1,5 @@
 const fs = require("fs").promises;
+const {Transform} = require("stream");
 
 //hw1
 // const read = fs.createReadStream("./input.txt", {
@@ -19,11 +20,35 @@ const fs = require("fs").promises;
 // });
 
 // //hw2
+// const obj ={
+//     a:"ա",
+//     b:"բ",
+//     c:"ց",
+//     d:"դ"
+// };
 // const read = fs.createReadStream("./../homework4Eng.txt", {
 //     highWaterMark: 1,
 //     encoding: "utf-8"
 // });
 //
+// const writeStream  = fs.createWriteStream("hw2.txt");
+
+//1 exanak read.on vercnel datan u chunkerov grel writeStream
+// read.on("data", (chunk => {
+//     writeStream.write(obj[chunk] || chunk)
+// }));
+
+//2exanak
+// const tr = new Transform({
+//     transform(chunk, encoding, next){
+//         this.push(obj[chunk] || chunk);
+//         next()
+//     }
+// });
+//
+// read.pipe(tr).pipe(writeStream);
+
+//3exanak
 // read.on("data", (chunk) => {
 //     // console.log(chunk);
 //     switch (chunk) {
@@ -51,13 +76,8 @@ const fs = require("fs").promises;
 // );
 
 // //hw3
-// const {Transform} = require("stream");
 //
 // class RemoveSpecialChars extends Transform {
-//     constructor(str) {
-//         super();
-//     }
-//
 //     _transform(chunk, encoding, next) {
 //         this.push(chunk.toString().replace(/[^a-z0-9]/gi, ""));
 //         next()
@@ -67,11 +87,10 @@ const fs = require("fs").promises;
 //
 // const rsc = new RemoveSpecialChars();
 // const rs = fs.createReadStream("./../homeworkw5.txt");
-// rs.pipe(rsc).pipe(fs.createWriteStream("./homeworkr50.txt"));
+// rs.pipe(rsc).pipe(fs.createWriteStream("./homeworkr500.txt"));
 
 
 //hw4
-
 function foo(str) {
     return new Promise((resolve,reject)=>{
         resolve(str=>str.split("/").map(x=>x[0].toUpperCase()+x.slice(1)).join(""))
@@ -82,3 +101,13 @@ foo("all/main/some/new").then((data)=>{
 }).catch((err)=>{
     console.log(err.message);
 });
+
+
+async function foo(path) {
+    const data = (await  fs.readFile(path)).toString();
+    // const newData = data.split(" ").map(x=>x[0].toUpperCase()+x.slice(1).toLowerCase()).join(" ");
+    await fs.writeFile("hw4.txt",data)
+}
+foo("./wr.txt")
+    .then(data=>console.log("data"))
+    .catch(err=> console.log(err.message));
