c5bf410db60611772567b0b82c13bcaa97339eca
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2021-10-15 18:01:17 +0400

    21

---
+++ 21homeWork/index.js
@@ -0,0 +1,11 @@
+// get, push, pull, patch, delete
+//stanal, uxarkel, popoxel, 1 popoxel, jnjel
+
+console.log("hi");
+
+const http = require("http");
+
+const server = http.createServer((req,res)=>{
+
+});
+server.listen(3000);
