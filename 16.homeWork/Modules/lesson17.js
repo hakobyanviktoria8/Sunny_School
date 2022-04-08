7106331c5c0e4852607dfe9348b3e2f3d024f2c7
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2022-04-08 17:09:26 +0400

    OS module

--- 16.homeWork/Modules/lesson17.js
+++ 16.homeWork/Modules/lesson17.js
@@ -14,17 +14,36 @@ const fs = require('fs')
 // server.listen(2021)
 
 
-// log 3
-fs.readFile("../.gitignore",function (err, data){
-    if(err){
-        return err.message
-    }
-    console.log(data.toString());
+// // log 3
+// fs.readFile("../.gitignore",function (err, data){
+//     if(err){
+//         return err.message
+//     }
+//     console.log(data.toString());
+// })
+
+// // blocking response,
+// log 1
+// const dataSync = fs.readFileSync("data.json","utf-8")
+// console.log(dataSync);
+
+// // log 2
+// console.log("test17");
+
+
+
+// // example 1
+// const readMe = fs.readFileSync("data.json","utf-8")
+// console.log(readMe);
+ 
+// fs.writeFileSync("writeSinc.txt", readMe)
+
+
+// example 2
+fs.readFile("../package.json", function(err,data){
+    if(err) return
+
+    console.log("Package json data here ", data.toString());
+
+    fs.writeFile("writeSinc.txt", data.toString(), ()=>console.log("writen"))
 })
-
-// blocking response, log 1
-const dataSync = fs.readFileSync("data.json","utf-8")
-console.log(dataSync);
-
-// log 2
-console.log("test17");
---
+++ 16.homeWork/Modules/os.js
@@ -0,0 +1,15 @@
+const os = require("os")
+
+// console.log(os);
+console.log(os.userInfo());
+
+// operating system platform
+console.log(os.platform());
+
+// operating system CPU architecture
+console.log(os.arch());
+
+console.log(os.cpus());
+
+console.log(os.networkInterfaces());
+
