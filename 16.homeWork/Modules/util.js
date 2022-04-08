7db3c3cdfdcff6833d6e69726e41599b80f5a742
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2022-04-08 20:50:41 +0400

    util module

---
+++ 16.homeWork/Modules/util.js
@@ -0,0 +1,17 @@
+// const util = require('util')
+// console.log(util);
+
+console.log(process.env.USER_ID);
+// const id = process.env.USER_ID
+// console.log(id);
+
+
+// const fs = require('fs')
+// const {promisify} = require('util')
+
+
+// const writeFile = promisify(fs.writeFile)
+
+// writeFile('some.txt','This is a some text')
+// .then(()=>console.log('success'))
+// .catch((err)=> console.log(err))
