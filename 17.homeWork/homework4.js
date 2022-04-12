a9026dc7e78707bca2b521304f82915588077191
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2022-04-12 17:57:29 +0400

    hw 17 4-5task

--- 17.homeWork/homework4.js
+++ 17.homeWork/homework4.js
@@ -3,15 +3,42 @@
 // Այնուհետև գծիկներով տեքստ գրել write.txt ֆայլում:
 //
 
-const fs = require("fs");
-
-fs.readFile("input.txt",'utf8',function (err,data) {
-    if (err) {throw err}
-    else {
-        // console.log(typeof data);  //string
-        // console.log(data.replace(" ", "-"));  //don't working
-
-        fs.writeFile("write.txt", data.split(" ").join("-"), err => err?.err.message);
-
+const fs = require('fs')
+fs.readFile('./input.txt', 'utf8' , (err, data) => {
+    if (err) {
+      console.error(err)
+      return
     }
-});
+    fs.writeFile("write111.txt",data.split(" ").join("-"), err=>{
+        err && console.log(err) 
+        return
+    })
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fs = require("fs");
+
+// fs.readFile("input.txt",'utf8',function (err,data) {
+//     if (err) {throw err}
+//     else {
+//         // console.log(typeof data);  //string
+//         // console.log(data.replace(" ", "-"));  //don't working
+
+//         fs.writeFile("write.txt", data.split(" ").join("-"), err => err?.err.message);
+
+//     }
+// });
--- 17.homeWork/homework5.js
+++ 17.homeWork/homework5.js
@@ -5,16 +5,44 @@
 // և այդ ֆայլում գրել նախորդ ֆայլերի պարունակությունը:
 
 
-const fs = require("fs");
-const os = require("os");
-const hour = new Date().getHours();
-const min = new Date().getMinutes();
-const sec = new Date().getSeconds();
-const name = `${os.platform()}_${hour}_${min}_${sec}`;
+const fs = require('fs')
+const os = require('os')
+let h = new Date().getHours()
+let m =  new Date().getMinutes()
+let s =  new Date().getSeconds()
+let n= os.platform()
+let name = `${n}_${h}_${m}_${s}.txt`
+const readFileHw = fs.readFileSync('./homework1.js') +  fs.readFileSync('./homework2.js')
 
-const dataFile = fs.readFileSync("./homework1.js")+fs.readFileSync("./homework2.js")+fs.readFileSync("./homework3.js")+fs.readFileSync("./homework4.js");
+fs.writeFileSync(name, readFileHw)   
 
-fs.writeFileSync(`${name}.txt`, dataFile);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fs = require("fs");
+// const os = require("os");
+// const hour = new Date().getHours();
+// const min = new Date().getMinutes();
+// const sec = new Date().getSeconds();
+// const name = `${os.platform()}_${hour}_${min}_${sec}`;
+
+// const dataFile = fs.readFileSync("./homework1.js")+fs.readFileSync("./homework2.js")+fs.readFileSync("./homework3.js")+fs.readFileSync("./homework4.js");
+
+// fs.writeFileSync(`${name}.txt`, dataFile);
 
 
 
